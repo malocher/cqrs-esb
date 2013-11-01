f97850b8e77ce9db58e7c989a599f2b8f5f405bf
Manfred Weber <manfred.weber@gmail.com>
2013-11-01 14:08:26 +0100

    tasks of event handlers

--- iterations/Iteration/Iteration1/Iteration1Handler.php
+++ iterations/Iteration/Iteration1/Iteration1Handler.php
@@ -15,18 +15,6 @@ use Cqrs\Adapter\AdapterTrait;
  *
  * Note the use of the AdapterTrait which loosely couples this file with the cqrs package
  *
- * „Command handler perform the following tasks:
- *
- * 1. It receives a command instance from the messaging infrastructure.
- * 2. It validates that the command is a valid command.
- * 3. It locates the aggregate instance that is the target of the command.
- *    This may involve creating a new aggregate instance or locating an existing instance.
- * 4. It invokes the appropriate method on the aggregate instance, passing in any parameters from the command.
- * 5. It persists the new state of the aggregate to storage.“
- *
- * „Exploring CQRS and Event Sourcing.“
- * iBooks. https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=85A96D9C4AEBE2F2D588B4683CCF608E
- *
  * @author Alexander Miertsch <kontakt@codeliner.ws>
  * @author Manfred Weber <crafics@php.net>
  * @package Iteration\Iteration1
@@ -36,6 +24,18 @@ class Iteration1Handler
     use AdapterTrait;
 
     /**
+     * „Command handler perform the following tasks:
+     *
+     * 1. It receives a command instance from the messaging infrastructure.
+     * 2. It validates that the command is a valid command.
+     * 3. It locates the aggregate instance that is the target of the command.
+     *    This may involve creating a new aggregate instance or locating an existing instance.
+     * 4. It invokes the appropriate method on the aggregate instance, passing in any parameters from the command.
+     * 5. It persists the new state of the aggregate to storage.“
+     *
+     * „Exploring CQRS and Event Sourcing.“
+     * iBooks. https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=85A96D9C4AEBE2F2D588B4683CCF608E
+     *
      * @param Iteration1Command $command
      */
     public function editCommand(Iteration1Command $command)
@@ -48,6 +48,16 @@ class Iteration1Handler
     }
 
     /**
+     * Events are published to multiple recipients, typically aggregate instances or process managers.
+     * The Event handler performs the following tasks:
+     *
+     * 1. It receives an Event instance from the messaging infrastructure.
+     * 2. It locates the aggregate or process manager instance that is the target of the event.
+     *    This may involve creating a new ag
+     *
+     * „Exploring CQRS and Event Sourcing.“
+     * iBooks. https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=85A96D9C4AEBE2F2D588B4683CCF608E
+     *
      * @param Iteration1Event $event
      */
     public function editEvent(Iteration1Event $event)
